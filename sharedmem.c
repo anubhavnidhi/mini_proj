77141258af25a35f1543e7cc2f8ec42e6b41830f
Tithy Sahu <tithys@arragon.cs.wisc.edu>
2016-02-01 14:12:44 -0600

    version2, using semaphores

--- sharedmem.c
+++ sharedmem.c
@@ -9,92 +9,48 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
+#include <semaphore.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <limits.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mainmutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t  empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t  fill = PTHREAD_COND_INITIALIZER;
-
-int maxBuf;
-int threads;
+#define MAXBUF 4*1024
 char *file;
-int *buffer;
-int full = 0;
-int use = 0;
-int count = 0;
 
-/*
- * Put item into shared buffer
- */
-void put(int value) {	
-	buffer[full] = value;
-	full = (full + 1) % maxBuf;
-	count++;
-}
-/*
- * Get item from shared buffer
- */
-int get() {
-	use = 0;
-	int tmp = buffer[use];
-	buffer[use] = INT_MAX; //???
-	//use = (use + 1) % maxBuf; //???
-	count--;
-	return tmp;
-}
+struct shmem {
+  sem_t mutex;
+  sem_t full;
+  sem_t empty;
+  char buffer[MAXBUF];
+} shmem;
+
+
 /*
  * Producer
  */
-void producer(void *arg) {
-	//int i;
-	//for (i = 0; i < maxBuf; i++) {
-		printf("Server role: producers: count %d maxBuf %d\n", count, maxBuf);
-		pthread_mutex_lock(&mutex); 
-		while (count == maxBuf) 
-			pthread_cond_wait(&empty, &mutex); 
-		pthread_mutex_unlock(&mutex);
-		put(* (int *) arg); 
-		pthread_mutex_lock(&mutex);
-		printf("Producer: Queued request count[%d]: %d \n", count, buffer[full-1]);
-		pthread_cond_signal(&fill); 
-		pthread_mutex_unlock(&mutex);
-	//}
+void producer() {
+	
 }
 
 /*
  * Consumer
  */
-void *consumer(void *arg) {
-	while(1){
-		printf("Client request = consumer \n");
-		pthread_mutex_lock(&mutex); 
-		while (count == 0) 
-			pthread_cond_wait(&fill, &mutex);
-		int rqst = get(); 
-		printf("Consumer: Client Request served: %d \n", rqst);
-		pthread_cond_signal(&empty);
-		pthread_mutex_unlock(&mutex); 
-	}
+void consumer() {
+	
 }
 
 /*
  * Get program arguments
  */
-void getargs(int *threads, int *maxBuf, char **file, int argc, char *argv[])
+void getargs(char **file, int argc, char *argv[])
 {
-    if (argc != 4) {
-	fprintf(stderr, "Usage: %s <threads> <buffer> <file>\n", argv[0]);
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 	exit(1);
     }
-    *threads = atoi(argv[1]);
-    *maxBuf = atoi(argv[2]);
-    *file = argv[3];
+    *file = argv[1];
 }
 
 /*
@@ -104,8 +60,9 @@ int main(int argc, char *argv[]){
 
 	//Initializations
 	int i,fd;
-	getargs(&threads, &maxBuf, &file, argc, argv);
-	printf("threads: %d, max buffer size: %d filename: %s\n", threads, maxBuf, file);
+	struct shmem *ptr;
+	getargs(&file, argc, argv);
+	printf("filename: %s\n", file);
         
 	//Shared memory using mmap
 	fd = open(file, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0660);
@@ -113,22 +70,42 @@ int main(int argc, char *argv[]){
 		fprintf(stderr,"Error opening file for writing");
 		return -1;
    	}
-	int result = write(fd, "", 1);
+	int result = write(fd, &shmem, 1);
 	if (result != 1) {
 		close(fd);
 		fprintf(stderr,"Error writing to file");
 		return -1;
 	}
     	// mmap the file
-	buffer = mmap(0, maxBuf, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (buffer == MAP_FAILED) {
+	ptr = (struct shmem *) mmap(0, sizeof(struct shmem), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (ptr == MAP_FAILED) {
 		close(fd);
 		fprintf(stderr,"Error mmapping the file");
 		return -1;
 	}
-
+	//initialize semaphores
+	if(sem_init(&ptr->mutex, 1, 1) != 0 )
+		fprintf(stderr,"sem_init error"), exit(-2);
+	if(sem_init(&ptr->empty, 0, MAXBUF) != 0 )
+		fprintf(stderr,"sem_init error"), exit(-2);
+	if(sem_init(&ptr->full, 0, 0) != 0 )
+		fprintf(stderr,"sem_init error"), exit(-2);
+	setbuf(stdout, NULL);
 	
-	munmap(0, maxBuf);
+	char *temp = (char*)malloc(MAXBUF);
+	for(i = 0; i < MAXBUF; i++)
+		*((char*)temp+i) = 'A';
+	
+	if (fork() == 0)
+		producer();
+	
+	//Measure Latency
+	//clock_gettime(CLOCK_MONOTONIC, &start);
+	consumer();
+	//clock_gettime(CLOCK_MONOTONIC, &end);
+
+	printf("Complete...\n");
+	munmap(0, MAXBUF);
 	close(fd);
 	return 0;
 }

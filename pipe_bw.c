08af6afa55963503ba3a53a6c1202b0a75fa642e
Anuhavnidhi <anuhavnidhi@cs.wisc.edu>
2016-02-04 14:32:49 -0600

    bw pipe done

--- pipe_bw.c
+++ pipe_bw.c
@@ -50,12 +50,16 @@ int main(int argc, char *argv[])
         return -1;
     }
     unsigned long STRINGSIZE = atoi(argv[1]);
-    int numPackets=atoi(argv[2]);
-    char buf[STRINGSIZE],iter=0;
+    int numPackets=1000;
+    int iterations=atoi(argv[2]);
+    printf("iter: %d\n",iterations);
+    char buf[STRINGSIZE];
     char s[1]="a";
-    unsigned long latency[numPackets];
+    unsigned long bw[iterations];
+    double min = 999999999999;
     int readpipe[2];
     int writepipe[2];
+    int iter=0;
     a=pipe(readpipe);
     b=pipe(writepipe);
     if (a == -1) { perror("pipe"); exit(EXIT_FAILURE); }
@@ -68,59 +72,73 @@ int main(int argc, char *argv[])
         printf("pid:main");
         exit(1);
     }
+    while(iter<iterations)
+    {
+    
+        if(pid==0)
+        { 
+                //printf("\nCHILD PROCESS");
+                close(readpipe[0]);
+                close(writepipe[1]);
+                memset( buf, 'a', sizeof(char)*STRINGSIZE );
+                buf[STRINGSIZE-1]='\0';
+            
+                int num = 0;
+                clock_gettime(CLOCK_MONOTONIC, &start);
+                    
+                while(num<numPackets)
+                {
+                    if(write(readpipe[1],buf,strlen(buf)+1) < 0)
+                    {
+                        printf("Child can't write\n");
+                    }
+                    //printf("CHILD %d\n",num);           
+                    num++;
+                }
+                if(read(writepipe[0],s,sizeof(s)) < 0)
+                    {
+                        printf("Parent can't read\n");
+                    }
+                  //  printf("CHILD READ\n");
+                clock_gettime(CLOCK_MONOTONIC, &stop);
+                remainderDelay = 1e9L * (stop.tv_sec - start.tv_sec) + stop.tv_nsec - start.tv_nsec;
+                //printf("%llu delay %d\n",remainderDelay,numPackets);
+                double micro = (remainderDelay/2000.0);
+                double bw = STRINGSIZE*numPackets/micro;
+                //printf("\nBW : %lf",bw);
+                if (bw<min)
+                    min = bw;
+               
+                //exit(0); 
+        }
+        else
+        { 
+                //printf("\nPAR PROCESS");
+                close(readpipe[1]);
+                close(writepipe[0]);
+                int num = 0;
+                while(num<numPackets)
+                {
+                    if(read(readpipe[0],buf,sizeof(buf)) < 0)
+                    {
+                        printf("Parent can't read\n");
+                    } 
+                    //printf("PAR %d: %s\n",num,buf); 
+                    num++;
+                }
+                if(write(writepipe[1],s,strlen(s)+1) < 0)
+                {
+                    printf("Parent can't write\n");
+                }
+                //exit(0);
+         
+        }
+        iter++;
+    }
+    
 
-    if(pid==0)
-    { 
-            //CHILD PROCESS
-            close(readpipe[0]);
-            close(writepipe[1]);
-            memset( buf, 'a', sizeof(char)*STRINGSIZE );
-            buf[STRINGSIZE-1]='\0';
-            int num = 0;
-            clock_gettime(CLOCK_MONOTONIC, &start);
-                
-            while(num<numPackets)
-            {
-                if(write(readpipe[1],buf,strlen(buf)+1) < 0)
-                {
-                    printf("Child can't write\n");
-                }
-                //printf("CHILD %d\n",num);           
-                num++;
-            }
-            if(read(writepipe[0],s,sizeof(s)) < 0)
-                {
-                    printf("Parent can't read\n");
-                }
-              //  printf("CHILD READ\n");
-            clock_gettime(CLOCK_MONOTONIC, &stop);
-            remainderDelay = 1e9L * (stop.tv_sec - start.tv_sec) + stop.tv_nsec - start.tv_nsec;
-            printf("%llu delay %d\n",remainderDelay,numPackets);
-            double micro = (remainderDelay/(double)numPackets)/(double)STRINGSIZE;
-            printf("\n%d %lf",STRINGSIZE,STRINGSIZE/micro);
-            exit(0); 
-    }
-    else
-    { 
-            //PARENT PROCESS
-            close(readpipe[1]);
-            close(writepipe[0]);
-            int num = 0;
-            while(num<numPackets)
-            {
-                if(read(readpipe[0],buf,sizeof(buf)) < 0)
-                {
-                    printf("Parent can't read\n");
-                } 
-                //printf("PAR %d: %s\n",num,buf); 
-                num++;
-            }
-            if(write(writepipe[1],s,strlen(s)+1) < 0)
-            {
-                printf("Parent can't write\n");
-            }
-            //printf("PAR WRITE\n");
-    }
+    printf("\n%d %lf\n",STRINGSIZE,min);
+    
     close(readpipe[1]);
     close(writepipe[0]);
     close(readpipe[0]);
